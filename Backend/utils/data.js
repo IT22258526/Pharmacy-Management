5533f938635f9b91b706b1a9fc134135eab9b071
IT22258526 <IT22258526@my.sliit.lk>
2024-05-08 23:21:47 +0530

    Create data.js

---
+++ Backend/utils/data.js
@@ -0,0 +1,34 @@
+const items =[
+    {   itemId:1,
+        name:'Panadol',
+        price:50.00,
+        category:'Tablets',
+        image:'https://unionchemistspharmacy.lk/wp-content/uploads/2022/11/Panadol-Tablets.jpg',
+
+    },
+    {   itemId:2,
+        name:'Reactin',
+        price:150.00,
+        category:'Tablets',
+        image:'https://www.fitbynet.com/wp-content/uploads/2018/11/reactin-diclofenac-sodium-tablets-500x500-1.webp'
+    },
+    
+    {
+        itemId:3,
+        name:'Gengigel',
+        price:500.00,
+        category:'Creams',
+        image:'https://i.ebayimg.com/images/g/FukAAOSwEgxggA7C/s-l225.jpg'
+    },
+    
+    {
+        itemId:4,
+        name:'Eugenol',
+        price:350.00,
+        category:'Syrups',
+        image:'https://www.prevestdenpro.com/wp-content/uploads/2021/08/Eugenol-min-1.jpg'
+    },
+   
+]
+
+module.exports = items;
